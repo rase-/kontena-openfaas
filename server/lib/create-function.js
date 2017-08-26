3cff52ef74ffdd9d035b6ad27bfd7d87d9203008
Tony Kovanen <tonykovanen@hotmail.com>
2017-08-26 22:48:02 +0300

    Transform Func to kontena spec

--- server/lib/create-function.js
+++ server/lib/create-function.js
@@ -3,9 +3,18 @@ import type KontenaApi from './kontena-api';
 
 const { json } = require('micro');
 
-function makeDeploymentSpec(body: CreateFunctionBody) {
-  // TODO
-  return {};
+function makeDeploymentSpec(body: CreateFunctionBody): Object {
+  return {
+    name: body.service,
+    image: body.image,
+    env: (body.envProcess && body.envProcess.length > 0
+        ? [`fprocess=${body.envProcess}`]
+        : []).concat(Object.keys(body.envVars).map((key) => {
+      return `${key}=${body.envVars[key]}`;
+    })),
+    strategy: 'ha',
+    ports: [8080]
+  };
 }
 
 module.exports = function(api: KontenaApi): (req: http$IncomingMessage, http$ServerResponse) => Promise<void> {
