00c2036d94bf64f6bfd9fea7003795158b111f07
Tony Kovanen <tonykovanen@hotmail.com>
2017-08-28 21:31:07 +0300

    await response stream closed while proxying

--- server/lib/proxy.js
+++ server/lib/proxy.js
@@ -2,8 +2,8 @@
 import type KontenaApi from './kontena-api';
 
 const { IncomingMessage } = require('http');
-
 const request = require('request');
+const streamToPromise = require('stream-to-promise');
 
 class ParamRequest extends IncomingMessage {
   params: { [string]: string };
@@ -17,5 +17,7 @@ module.exports = function(api: KontenaApi): (req: ParamRequest, http$ServerRespo
     req
       .pipe(request.post(`http://${name}.${api.gridId}.kontena.local:8080`))
       .pipe(res);
+
+    await streamToPromise(res);
   }
 }
